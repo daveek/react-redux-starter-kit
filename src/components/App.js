63ea19f1f033e30e77f451ee183562021ed40456
Dave Rojas <dave.rojas@ie.edu>
2017-09-21 16:24:15 +0200

    Cards github component at home

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { browserHistory, Router } from 'react-router'
 import { Provider } from 'react-redux'
 import PropTypes from 'prop-types'
+import axios from 'axios'
 
 class App extends React.Component {
   static propTypes = {
--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -1,6 +1,7 @@
 import HomeView from './components/HomeView'
+import CardView from './components/CardView'
 
 // Sync route definition
 export default {
-  component : HomeView
+  component : CardView
 }
